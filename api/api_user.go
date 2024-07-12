54e259f334c28996067df344ddeb55be7a2aebd8
tantianran <15915822634@139.com>
2024-07-12 14:25:49 +0800

    optimize user

--- api/api_user.go
+++ api/api_user.go
@@ -3,6 +3,7 @@ package api
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	"github.com/marsacotan/go-zabbix7/types"
@@ -10,83 +11,68 @@ import (
 
 // Get a token.
 func (u *UserAPI) GetToken() (types.UserLoginTokenResponse, error) {
-
-	reqBody := types.UserLoginRequest{
-		JSONRPC: "2.0",
-		Method:  "user.login",
-		Params: types.UserLoginParams{
-			Username: u.Config.User,
-			Password: u.Config.Passwd,
-		},
-		ID: 1,
-	}
-
-	reqBytes, err := json.Marshal(reqBody)
+	resp, err := u.makeRequest(false)
 	if err != nil {
 		return types.UserLoginTokenResponse{}, err
 	}
-
-	req, err := http.NewRequest("POST", u.Config.URL, bytes.NewBuffer(reqBytes))
-	if err != nil {
-		return types.UserLoginTokenResponse{}, err
-	}
-
-	req.Header.Set("Content-Type", "application/json-rpc")
-
-	resp, err := u.Client.Do(req)
-
-	if err != nil {
-		return types.UserLoginTokenResponse{}, err
-	}
-
-	if resp.StatusCode != http.StatusOK {
-		return types.UserLoginTokenResponse{}, err
-	}
-
-	defer resp.Body.Close()
-
-	var resBody types.UserLoginTokenResponse
-	if err := json.NewDecoder(resp.Body).Decode(&resBody); err != nil {
-		return types.UserLoginTokenResponse{}, err
-	}
-	return resBody, nil
+	return resp.(types.UserLoginTokenResponse), nil
 }
 
 // Get other information about the authenticated user.
 func (u *UserAPI) GetUserData() (types.UserLoginUserDataResponse, error) {
+	resp, err := u.makeRequest(true)
+	if err != nil {
+		return types.UserLoginUserDataResponse{}, err
+	}
+	return resp.(types.UserLoginUserDataResponse), nil
+}
 
+// makeRequest makes a request based on userData parameter.
+func (u *UserAPI) makeRequest(userData bool) (interface{}, error) {
 	reqBody := types.UserLoginRequest{
 		JSONRPC: "2.0",
 		Method:  "user.login",
 		Params: types.UserLoginParams{
 			Username: u.Config.User,
 			Password: u.Config.Passwd,
-			UserData: true,
+			UserData: userData,
 		},
 		ID: 1,
 	}
 
 	reqBytes, err := json.Marshal(reqBody)
 	if err != nil {
-		return types.UserLoginUserDataResponse{}, err
+		return nil, err
 	}
 
 	req, err := http.NewRequest("POST", u.Config.URL, bytes.NewBuffer(reqBytes))
 	if err != nil {
-		return types.UserLoginUserDataResponse{}, err
+		return nil, err
 	}
+
 	req.Header.Set("Content-Type", "application/json-rpc")
 
 	resp, err := u.Client.Do(req)
-
 	if err != nil {
-		return types.UserLoginUserDataResponse{}, err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
-	var resBody types.UserLoginUserDataResponse
-	if err := json.NewDecoder(resp.Body).Decode(&resBody); err != nil {
-		return types.UserLoginUserDataResponse{}, err
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
-	return resBody, nil
+
+	if userData {
+		var resBody types.UserLoginUserDataResponse
+		if err := json.NewDecoder(resp.Body).Decode(&resBody); err != nil {
+			return nil, err
+		}
+		return resBody, nil
+	} else {
+		var resBody types.UserLoginTokenResponse
+		if err := json.NewDecoder(resp.Body).Decode(&resBody); err != nil {
+			return nil, err
+		}
+		return resBody, nil
+	}
 }
--- api/client.go
+++ api/client.go
@@ -2,19 +2,12 @@ package api
 
 import "github.com/marsacotan/go-zabbix7/utils"
 
-// Creating an http client is secure by default, which means TLS verification is required.
-// The default value of config.InsecureSkipVerify is False, which returns an http client with TLS verification enabled.
-// If you want to skip TLS verification, you need to set it to True
 func NewClient(config *Config) *API {
-	if !config.InsecureSkipVerify {
-		httpClient := utils.CreateTLSVerifyHTTPClient(config.CertPath)
-		return &API{
-			Config: config,
-			User:   UserAPI{Config: config, Client: httpClient},
-			Token:  TokenAPI{Config: config, Client: httpClient},
-		}
-	}
 	httpClient := utils.CreateHTTPClient()
+	if !config.InsecureSkipVerify {
+		httpClient = utils.CreateTLSVerifyHTTPClient(config.CertPath)
+	}
+
 	return &API{
 		Config: config,
 		User:   UserAPI{Config: config, Client: httpClient},
